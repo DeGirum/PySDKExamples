cae2813f6ad05a6034532d09e3090e975e253ed8
Gowna Mohanasundaram <gowna@degirum.com>
2023-08-21 14:51:25 -0700

    updated evaluation script with model zoo and token as parameter

--- mAP_evaluation_test.py
+++ mAP_evaluation_test.py
@@ -1,30 +1,13 @@
 import argparse
 import json
-import degirum as dg, mytools
-import requests
-import os
-import tempfile
-from contextlib import contextmanager
 from dg_coco_utils import *
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 from tqdm import tqdm
 import numpy as np
 import csv
+import degirum as dg
 
-@contextmanager
-def annotations_accessor(url:str):
-    r = requests.get(url)
-    r.raise_for_status()
-    fd, fname = tempfile.mkstemp()
-    try:
-        with os.fdopen(fd, 'w') as f:
-            json.dump(r.json(), f)
-        yield fname
-    finally:
-        os.unlink(fname)
-        
-        
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
 #     parser.add_argument('--save_pred_path', type=str, help='save the predictions as a json')
@@ -38,27 +21,38 @@ def parse_opt(known=False):
     parser.add_argument('--InputPadMethod', default='letterbox', help='InputPadMethod')
     parser.add_argument('--ImageBackend',type=str, default='opencv', help='ImageBackend')
     parser.add_argument('--InputImgFmt',type=str, default='JPEG', help='format of the input image')
+    parser.add_argument('--save_mAP_path',type=str, default='./mAP_results.csv', help='saving the results to a csv file')
+    parser.add_argument('--cloud_zoo_url',type=str, default='public', help='model zoo url from the Degirum cloud zoo (https://cs.degirum.com/)')
+    parser.add_argument('--cloud_token',type=str, default='dg_AMJjLaPG4YBYPcCDfUMKPJbrDmSoJSjLJruYw', help='generate a token from My Tokens in https://cs.degirum.com/')
+    
+    
 #     parser.add_argument('--ClassMap', type=str, default = 'coco', help='classmap for each category')
 
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
 
-def main(opt, models):
+def main(opt):
+    
+    models = dg.connect(dg.CLOUD, "https://cs.degirum.com/degirum/" + opt.cloud_zoo_url, opt.cloud_token) # get all the models from the model zoo
+    
     with open('./coco_configs/classmap_config.json') as config:
         classmap_categories=json.load(config)
-    
-#     print (models.list_models())
-    
-    with open('./models.json') as json_data:
-        models_list=json.load(json_data)
+        
+#     with open('./models_test.json' , "r") as json_data:
+#         model_zoo=json.load(json_data)
+#     models_list = models[opt.cloud_zoo_url]
         
     with open('./coco_configs/input_data.json') as df:
         input_json = json.load(df)
     
     
     mAP_list = []
-    for key,model_name in models_list.items():
-        print (model_name)
+    models_list = models.list_models(model_family="yolo")
+#     print (models_list)
+    for i in range(len(models_list)):
+        model_name = models_list[i]
+        print (i,model_name)
+        
         if "coco" in model_name:
             image_folder_path = input_json["image_folder_path"]["coco"]
             ground_truth_annotations_path = input_json["ground_truth_annotations_path"]["coco"]
@@ -99,8 +93,7 @@ def main(opt, models):
         eval_model.image_backend=opt.ImageBackend
         eval_model.input_image_format=opt.InputImgFmt
         jdict=[]
-        with annotations_accessor(ground_truth_annotations_path) as annotations_file:
-            anno = COCO(annotations_file)
+        anno = COCO(ground_truth_annotations_path)
         num_images=len(anno.dataset['images'])
         files_dict=anno.dataset['images'][0:num_images]
         path_list=[]
@@ -110,9 +103,10 @@ def main(opt, models):
             path=image_folder_path + files_dict[image_number]['file_name']
             path_list.append(path)
         with eval_model:
-            for image_number,predictions in tqdm(enumerate(eval_model.predict_batch(path_list))):
-                if image_number%print_frequency==print_frequency-1:
-                    print(image_number+1)
+            for image_number,predictions in enumerate(eval_model.predict_batch(path_list)):
+#                 if image_number%print_frequency==print_frequency-1:
+#                     image_number = image_number+1
+#                     print(image_number+1)
                 image_id=files_dict[image_number]['id']
                 save_results_coco_json(predictions.results,jdict,image_id,class_map)
         with open(pred_path, 'w') as f:
@@ -123,15 +117,15 @@ def main(opt, models):
         eval_obj.params.imgIds = [file['id'] for file in files_dict]  # image IDs to evaluate
         eval_obj.evaluate()
         eval_obj.accumulate()
-        eval_obj.summarize()
+#         eval_obj.summarize()
         map_all, map50 = eval_obj.stats[:2]  # update results (mAP@0.5:0.95, mAP@0.5)
-        print ("------------------saving the results to a csv file-----------------------")
+        print ("------------------Saving the results to a csv file-----------------------")
         header = ['model_name','mAP50', 'mAP75', 'mAP0.50:0.95', 'mAP0.50-0.95','mAP0.50-0.95','mAP0.50-0.95','mAP0.50-0.95','mAP0.50-0.95','mAP0.50-0.95', 'mAP0.50-0.95','mAP0.50-0.95']
         data = eval_obj.stats.tolist()
         data.insert(0,model_name)
         mAP_list.append(data)
         
-        with open('./map_results.csv', 'w', encoding='UTF8', newline ='') as f:
+        with open(opt.save_mAP_path, 'w', encoding='UTF8', newline ='') as f:
             writer = csv.writer(f, delimiter=',') 
             writer.writerow(header)
             writer.writerows(mAP_list)
@@ -140,7 +134,4 @@ def main(opt, models):
 
 if __name__ == '__main__':
     opt = parse_opt()
-    cloud_token = mytools.get_token() # get cloud API access token from env.ini file
-    cloud_zoo_url = mytools.get_cloud_zoo_url() # get cloud zoo URL from env.ini file
-    zoo = dg.connect(dg.CLOUD, cloud_zoo_url, cloud_token)
-    main(opt, zoo)
+    main(opt)
