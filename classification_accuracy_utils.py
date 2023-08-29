88208a04f6676863fe3edfcdd6a73ebc96da0553
Gowna Mohanasundaram <gowna@degirum.com>
2023-08-29 11:21:36 -0700

    updated image classification accuracy evaluation

--- classification_accuracy_utils.py
+++ classification_accuracy_utils.py
@@ -5,7 +5,7 @@ import csv
 import os
 from random import shuffle
 
-class ClassificationModelAccuracyEvaluator:
+class ImageClassificationModelEvaluator:
     
     def __init__(
             self,
@@ -13,7 +13,11 @@ class ClassificationModelAccuracyEvaluator:
             image_folder_path,
             image_label_map_json,
             num_classes,
-            k,
+            top_k,
+            input_resize_method="bilinear",
+            input_pad_method="letterbox",
+            image_backend="opencv",
+            input_img_fmt="JPEG",
             print_frequency=500
       ):
     
@@ -24,9 +28,9 @@ class ClassificationModelAccuracyEvaluator:
             Args:
                 dg_model (Detection model): Classification model from the Degirum model zoo.
                 image_folder_path (str): Path to the image dataset.
-                image_labekl_map_json (str) : Path to the image-label-map json file.
+                image_label_map_json (str) : Path to the image-label-map json file.
                 num_classes (int) : Number of classes in the test dataset (Example: ImageNet has 1000 classes).
-                k (int) : Th value of `k` in top-k.
+                k (int) : The value of `k` in top-k.
                 print_frequency (int): Number of image batches to be evaluated at a time.
 
         """
@@ -35,9 +39,21 @@ class ClassificationModelAccuracyEvaluator:
         self.image_folder_path = image_folder_path
         self.image_label_map_json = image_label_map_json
         self.num_classes = num_classes
-        self.k = k
+        self.top_k = top_k
+        self.input_resize_method = input_resize_method
+        self.input_pad_method = input_pad_method
+        self.image_backend = image_backend
+        self.input_img_fmt = input_img_fmt
         self.print_frequency = print_frequency
-     
+        
+        
+        if (self.dg_model.output_postprocess_type == "Classification"): 
+            self.dg_model.input_resize_method = self.input_resize_method
+            self.dg_model.input_pad_method = self.input_pad_method
+            self.dg_model.image_backend = self.image_backend
+            self.dg_model.input_image_format = self.input_img_fmt
+        else:
+            raise Exception("Model loaded for evaluation is not a Classification Model")
     
     def load_images_and_label_map(self):
         
@@ -63,38 +79,42 @@ class ClassificationModelAccuracyEvaluator:
         image_folder_path = load_yaml["ImageFolderPath"]
         image_label_map_json = load_yaml["ImageLabelMap"]
         num_classes = load_yaml["NumClasses"]
-        k = load_yaml["K"]
+        top_k = load_yaml["TopK"]
+        input_resize_method = load_yaml["InputResizeMethod"]
+        input_pad_method = load_yaml["InputPadMethod"]
+        image_backend = load_yaml["ImageBackend"]
+        input_image_format = load_yaml["InputImgFmt"]
         print_frequency = load_yaml["PrintFrequency"]
         
-        return cls(dg_model, image_folder_path, image_label_map_json, num_classes, k, print_frequency)
+        return cls(dg_model, image_folder_path, image_label_map_json, num_classes, top_k, input_resize_method, input_pad_method, image_backend, input_image_format, print_frequency)
         
-    def top_k_accuracy_evaluate(self):
+    def evaluate(self):
         top_one_accuracy = 0
         top_k_accuracy = 0
         inference_count = 0
         test_images,image_label_map = self.load_images_and_label_map()
         num_output_classes = len(self.dg_model.label_dictionary)
-        if self.k > 0 and self.k <= num_output_classes:
-            self.dg_model.output_top_k = int(self.k)
+        if self.top_k > 0 and self.top_k <= num_output_classes:
+            self.dg_model.output_top_k = int(self.top_k)
             
         for test_img, test_img_path in test_images:  # Calculate top-1 and top-k accuracies
             category_id = image_label_map[test_img]  # O(1) look-up 
 #             print("Inferencing image {}, {}".format(inference_count, test_img_path))
             result = self.dg_model(test_img_path).results
             if len(result) > 0:
-                max_category_id = None
+                top_category_id = None
                 max_score = -10e5
-                category_ids = [res_obj['category_id'] + self.num_classes - num_output_classes for res_obj in result]
-                max_category_id = result[0]['category_id'] + self.num_classes - num_output_classes
+                top_k_category_ids = [res_obj['category_id'] + self.num_classes - num_output_classes for res_obj in result]
+                top_category_id = result[0]['category_id'] + self.num_classes - num_output_classes
                 if category_id == max_category_id:
                     top_one_accuracy += 1
                     top_k_accuracy += 1
-                elif category_id in category_ids:
+                elif category_id in top_k_category_ids:
                     top_k_accuracy += 1
                 inference_count += 1
                 if inference_count % self.print_frequency == 0:
                     print ("Inferencing image {}".format(inference_count))
-                    print ("True class: {}, Predicted class: {}".format(category_id, max_category_id))
+                    print ("True class: {}, Predicted class: {}".format(category_id, top_category_id))
                     print ("Top-1 accuracy: {}, Top-{} accuracy: {}".format(top_one_accuracy * 100 / inference_count, self.dg_model.output_top_k, top_k_accuracy * 100 / inference_count))
                     print("\n")
         print("Top-1 accuracy is {}".format(top_one_accuracy * 100 / inference_count))
