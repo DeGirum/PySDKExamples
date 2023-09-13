559abcee419482136a718a8faa7b73da0f327565
Gowna Mohanasundaram <gowna@degirum.com>
2023-09-12 20:02:17 -0700

    person reid evaluation on Market-1501 dataset

---
+++ mAP_reid_utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def evaluate(qf,ql,qc,gf,gl,gc):
+    query = qf
+    score = gf@query 
+    index = np.argsort(score)[::-1] 
+    query_index = np.argwhere(gl==ql)
+    camera_index = np.argwhere(gc==qc) 
+    good_index = np.setdiff1d(query_index, camera_index, assume_unique=True)     
+    junk_index1 = np.argwhere(gl==-1)
+    junk_index2 = np.intersect1d(query_index, camera_index)
+    junk_index = np.append(junk_index2, junk_index1) 
+    CMC_tmp = compute_mAP(index, good_index, junk_index)
+    return CMC_tmp
+
+def compute_mAP(index, good_index, junk_index):
+    ap = 0
+    cmc = np.zeros(len(index), dtype=int)
+    if good_index.size==0:   # if empty
+        cmc[0] = -1
+        return ap,cmc
+
+    # remove junk_index
+    mask = np.in1d(index, junk_index, invert=True)
+    index = index[mask]
+
+    # find good_index index
+    ngood = len(good_index)
+    mask = np.in1d(index, good_index)
+    rows_good = np.argwhere(mask==True)
+    rows_good = rows_good.flatten()
+    
+    cmc[rows_good[0]:] = 1
+    for i in range(ngood):
+        d_recall = 1.0/ngood
+        precision = (i+1)*1.0/(rows_good[i]+1)
+        if rows_good[i]!=0:
+            old_precision = i*1.0/rows_good[i]
+        else:
+            old_precision=1.0
+        ap = ap + d_recall*(old_precision + precision)/2
+    return ap, cmc
+
