c8916569b49030365248843b5926ff3a48938d9b
gowna95m <137097011+gowna95m@users.noreply.github.com>
2023-09-21 18:00:38 -0700

    age-gender model evaluation
    Signed-off-by: gowna95m <137097011+gowna95m@users.noreply.github.com>

---
+++ age_gender_evaluation.py
@@ -0,0 +1,74 @@
+import json
+import os
+import yaml
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import accuracy_score
+
+class AgeGenderRecognitionModelEvaluator:
+    
+    def __init__(
+        self,
+        dg_model,
+        image_folder_path,
+        ground_truth_annotations_path
+    ):
+        """
+        Constructor.
+            This class evaluates the Accuracy and Mean Absolute Error for Age Gender Recognition models.
+
+            Args:
+                dg_model (model): Age-Gender model from the Degirum model zoo.
+                image_folder_path (str): Path to the image dataset.
+                ground_truth_annotations_path (str): Path to the groundtruth json annotations.
+        """
+
+        self.dg_model = dg_model
+        self.image_folder_path = image_folder_path
+        self.ground_truth_annotations_path = ground_truth_annotations_path
+        
+    @classmethod
+    def init_from_yaml(cls, dg_model, config_yaml):
+        """
+        args_yaml (str) : Path of the yaml file that contains all the arguments.
+
+        """
+        with open(config_yaml) as f:
+            load_yaml = yaml.load(f, Loader=yaml.FullLoader)
+
+        image_folder_path = load_yaml["ImageFolderPath"]
+        ground_truth_annotations_path = load_yaml["GroundTruthAnnotationsPath"]
+
+        return cls(
+            dg_model,
+            image_folder_path,
+            ground_truth_annotations_path
+        )
+    
+    def evaluate(self):
+        """Evaluation for the Age Gender Recognition model.
+
+        Returns the Accuracy for Gender and Mean Absolute Error for Age.
+        """
+        
+        with open(self.ground_truth_annotations_path, 'r') as json_file:
+            data = json.load(json_file)
+        age_groundtruth=[]
+        gender_groundtruth=[]
+        for i in data:
+            age_groundtruth.append(int(i["age"]))
+            gender_groundtruth.append(int(i["gender"])) 
+            
+        gender_predictions = []
+        age_predictions = []
+        for image_name in os.listdir(self.image_folder_path):
+            image_path = os.path.join(self.image_folder_path, image_name)
+            res=self.dg_model(image_path)
+            if res.results[0]["data"][0][0][0][0] >  res.results[0]["data"][0][1][0][0]:
+                gender_predictions.append(1)
+            else:
+                gender_predictions.append(0)
+            age = int (res.results[1]["data"][0][0][0][0] * 100)
+            age_predictions.append(age)
+        gender_accuracy = accuracy_score(gender_predictions, gender_groundtruth)
+        mae_age = mean_absolute_error(age_predictions, age_groundtruth)
+        return (gender_accuracy,mae_age)
