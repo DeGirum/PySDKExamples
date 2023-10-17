a3abc33f885445d3d07cc41bb5e39f1ce5a805b0
Mehrdad <khatami-mehrdad@github.com>
2023-10-16 20:28:43 -0700

    run coco

--- benchmark_scripts/detection_eval.py
+++ benchmark_scripts/detection_eval.py
@@ -18,18 +18,23 @@ cloud_zoo_url = mytools.get_cloud_zoo_url() # get cloud zoo URL from env.ini fil
 zoo = dg.connect(dg.CLOUD, cloud_zoo_url, cloud_token)
 #
 # print( zoo.list_models() )
-model_name='yolov8s_relu6_fruits--512x512_float_openvino_cpu_1'
-model_name='visdrone_yolov5n_relu6--640x640_float_openvino_cpu_1'
+# model_name='yolov8s_relu6_fruits--512x512_float_openvino_cpu_1'
+# model_name='visdrone_yolov5n_relu6--640x640_float_openvino_cpu_1'
+model_name='yolov5nu_relu6_coco--640x640_quant_n2x_orca1_1'
+# model_name='yolov5nu_relu6_car--640x640_quant_n2x_orca1_1'
 
 model=zoo.load_model(model_name)
 
 map_evaluator=ObjectDetectionModelEvaluator(model, input_img_fmt="JPEG")
 
-img_folder_path = '/data/ml-data/dataset_51/exported_dataset/trncFruits/images/validation'
-anno_json = '/home/mehrdad/wa/DG/PySDKExamples/annotations.json'
+# img_folder_path = '/data/ml-data/dataset_51/exported_dataset/trncFruits/images/validation'
+# anno_json = '/home/mehrdad/wa/DG/PySDKExamples/annotations.json'
 
-img_folder_path = '/data/VisDrone/VisDrone2019-DET-val/images'
-anno_json = '/home/mehrdad/wa/DG/PySDKExamples/visdrone_labels.json'
+# img_folder_path = '/data/VisDrone/VisDrone2019-DET-val/images'
+# anno_json = '/home/mehrdad/wa/DG/PySDKExamples/visdrone_labels.json'
 
-map_results=map_evaluator.evaluate(img_folder_path, ground_truth_annotations_path=anno_json, print_frequency=50)
+img_folder_path = '/data/ml-data/datasets/coco/images/val2017'
+anno_json = '/home/mehrdad/wa/DG/PySDKExamples/coco_labels.json'
+
+map_results=map_evaluator.evaluate(img_folder_path, ground_truth_annotations_path=anno_json, num_val_images=10, print_frequency=100)
 
