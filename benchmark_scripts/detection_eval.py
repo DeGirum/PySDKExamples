38c360f08afe3d4ff11901de7dd88dc418607b37
Mehrdad <khatami-mehrdad@github.com>
2023-10-25 14:01:27 -0700

    reformatted

--- benchmark_scripts/detection_eval.py
+++ benchmark_scripts/detection_eval.py
@@ -1,25 +1,107 @@
-
 import degirum as dg
 import dgtools
 from dgtools.detection_eval import ObjectDetectionModelEvaluator
 
-cloud_token = dgtools.get_token() # get cloud API access token from env.ini file
-cloud_zoo_url = dgtools.get_cloud_zoo_url() # get cloud zoo URL from env.ini file
+cloud_token = dgtools.get_token()  # get cloud API access token from env.ini file
+cloud_zoo_url = dgtools.get_cloud_zoo_url()  # get cloud zoo URL from env.ini file
 
 zoo = dg.connect(dg.CLOUD, cloud_zoo_url, cloud_token)
 
-model_name = 'yolo_v5s_coco--512x512_quant_n2x_orca_1'
+model_name = "yolo_v5s_coco--512x512_quant_n2x_orca_1"
 
 model = zoo.load_model(model_name)
 
-classmap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34,
-          35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-          64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90] 
+classmap = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    27,
+    28,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    46,
+    47,
+    48,
+    49,
+    50,
+    51,
+    52,
+    53,
+    54,
+    55,
+    56,
+    57,
+    58,
+    59,
+    60,
+    61,
+    62,
+    63,
+    64,
+    65,
+    67,
+    70,
+    72,
+    73,
+    74,
+    75,
+    76,
+    77,
+    78,
+    79,
+    80,
+    81,
+    82,
+    84,
+    85,
+    86,
+    87,
+    88,
+    89,
+    90,
+]
 
-map_evaluator = ObjectDetectionModelEvaluator(model, classmap = classmap)
+map_evaluator = ObjectDetectionModelEvaluator(model, classmap=classmap)
 
-img_folder_path = '/data/ml-data/datasets/coco/images/val2017'
-anno_json = '/data/ml-data/datasets/coco/annotations/instances_val2017.json'
+img_folder_path = "/data/ml-data/datasets/coco/images/val2017"
+anno_json = "/data/ml-data/datasets/coco/annotations/instances_val2017.json"
 
-map_results = map_evaluator.evaluate(img_folder_path, ground_truth_annotations_path = anno_json, num_val_images = 0, print_frequency = 100)
-
+map_results = map_evaluator.evaluate(
+    img_folder_path,
+    ground_truth_annotations_path=anno_json,
+    num_val_images=0,
+    print_frequency=100,
+)
--- benchmark_scripts/detection_eval_yaml.py
+++ benchmark_scripts/detection_eval_yaml.py
@@ -1,21 +1,26 @@
-
 import degirum as dg
 import dgtools
 from dgtools.detection_eval import ObjectDetectionModelEvaluator
 
-cloud_token = dgtools.get_token() # get cloud API access token from env.ini file
-cloud_zoo_url = dgtools.get_cloud_zoo_url() # get cloud zoo URL from env.ini file
+cloud_token = dgtools.get_token()  # get cloud API access token from env.ini file
+cloud_zoo_url = dgtools.get_cloud_zoo_url()  # get cloud zoo URL from env.ini file
 
 zoo = dg.connect(dg.CLOUD, cloud_zoo_url, cloud_token)
 
-model_name = 'yolo_v5s_coco--512x512_quant_n2x_orca_1'
+model_name = "yolo_v5s_coco--512x512_quant_n2x_orca_1"
 
 model = zoo.load_model(model_name)
 
-map_evaluator = ObjectDetectionModelEvaluator.init_from_yaml(model, 'benchmark_scripts/eval_yaml/coco.yaml')
+map_evaluator = ObjectDetectionModelEvaluator.init_from_yaml(
+    model, "benchmark_scripts/eval_yaml/coco.yaml"
+)
 
-img_folder_path = '/data/ml-data/datasets/coco/images/val2017'
-anno_json = '/data/ml-data/datasets/coco/annotations/instances_val2017.json'
+img_folder_path = "/data/ml-data/datasets/coco/images/val2017"
+anno_json = "/data/ml-data/datasets/coco/annotations/instances_val2017.json"
 
-map_results = map_evaluator.evaluate(img_folder_path, ground_truth_annotations_path = anno_json, num_val_images = 0, print_frequency = 100)
-
+map_results = map_evaluator.evaluate(
+    img_folder_path,
+    ground_truth_annotations_path=anno_json,
+    num_val_images=0,
+    print_frequency=100,
+)
