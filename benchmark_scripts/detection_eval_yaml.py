3789588fb666f5764b91c14dc9bd20147bbc4a8a
Mehrdad <khatami-mehrdad@github.com>
2023-10-24 23:04:58 -0700

    eval files

--- benchmark_scripts/detection_eval_yaml.py
+++ benchmark_scripts/detection_eval_yaml.py
@@ -12,11 +12,7 @@ model_name = 'yolo_v5s_coco--512x512_quant_n2x_orca_1'
 
 model = zoo.load_model(model_name)
 
-classmap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34,
-          35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-          64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90] 
-
-map_evaluator = ObjectDetectionModelEvaluator(model, classmap = classmap)
+map_evaluator = ObjectDetectionModelEvaluator.init_from_yaml(model, 'benchmark_scripts/eval_yaml/coco.yaml')
 
 img_folder_path = '/data/ml-data/datasets/coco/images/val2017'
 anno_json = '/data/ml-data/datasets/coco/annotations/instances_val2017.json'
