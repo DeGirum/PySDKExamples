a815d10f1b576aa12f9e797fc3d022b4107b8852
Mehrdad <khatami-mehrdad@github.com>
2023-10-25 19:33:00 -0700

    detection eval as a function

--- benchmark_scripts/detection_eval_yaml.py
+++ benchmark_scripts/detection_eval_yaml.py
@@ -1,3 +1,5 @@
+
+import argparse
 import degirum as dg
 import dgtools
 from dgtools.detection_eval import ObjectDetectionModelEvaluator
@@ -7,20 +9,35 @@ cloud_zoo_url = dgtools.get_cloud_zoo_url()  # get cloud zoo URL from env.ini fi
 
 zoo = dg.connect(dg.CLOUD, cloud_zoo_url, cloud_token)
 
-model_name = "yolo_v5s_coco--512x512_quant_n2x_orca_1"
+def validate(model_name:str, 
+            img_folder_path:str, 
+            anno_json:str, 
+            cfg_yaml:str="benchmark_scripts/eval_yaml/default.yaml"
+            ):
+    model = zoo.load_model(model_name)
+    map_evaluator = ObjectDetectionModelEvaluator.init_from_yaml(
+        model, cfg_yaml
+    )
+    map_results = map_evaluator.evaluate(
+        img_folder_path,
+        ground_truth_annotations_path=anno_json,
+        num_val_images=0,
+        print_frequency=100,
+    )
 
-model = zoo.load_model(model_name)
+    return map_results
 
-map_evaluator = ObjectDetectionModelEvaluator.init_from_yaml(
-    model, "benchmark_scripts/eval_yaml/coco.yaml"
-)
+def parser_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='', help='model name')
+    parser.add_argument('--data', type=str, help='path to validation images folder')
+    parser.add_argument('--annotations', type=str, help='ground truth annotation json file path')
+    parser.add_argument('--cfg', type=str, default='benchmark_scripts/eval_yaml/default.yaml', help='path to eval config')
 
-img_folder_path = "/data/ml-data/datasets/coco/images/val2017"
-anno_json = "/data/ml-data/datasets/coco/annotations/instances_val2017.json"
+    return parser.parse_args()
 
-map_results = map_evaluator.evaluate(
-    img_folder_path,
-    ground_truth_annotations_path=anno_json,
-    num_val_images=0,
-    print_frequency=100,
-)
+if __name__ == '__main__':
+
+    args = parser_arguments()
+    
+    validate(model_name=args.model, img_folder_path=args.data, anno_json=args.annotations, cfg_yaml=args.cfg)
