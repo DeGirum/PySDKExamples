b76214644582768a25f6dadd51f055eb4b53e541
shashichilappagari <36974899+shashichilappagari@users.noreply.github.com>
2023-12-09 09:54:06 -0800

    fixing unit tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -46,7 +46,7 @@ output_dir.mkdir(exist_ok=True)
 _image_notebooks = [
     ("basic/object_detection_video_stream.ipynb", "Masked.mp4", [3], []),
     ("basic/object_detection_image.ipynb", None, [3], []),
-    ("dgstreams/dgstreams_demo.ipynb", "Masked.mp4", [2, 3, 4, 5, 6,7], []),
+    ("dgstreams/dgstreams_demo.ipynb", "Masked.mp4", [3, 4, 5, 6, 8, 10], []),
     ("dgstreams/multi_camera_multi_model_detection.ipynb", "Masked.mp4", [4], []),
     ("dgstreams/person_pose_detection_pipelined_video_stream.ipynb", "Masked.mp4", [5], []),
     ("multimodel/face_hand_detection_parallel_video_stream.ipynb", "Masked.mp4", [3], []),
